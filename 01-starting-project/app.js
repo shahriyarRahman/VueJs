b903924ec803e5019ac8b6e5e013e4746057ff2d
Shahriyar Rahman <58167217+shahriyarRahman@users.noreply.github.com>
2021-01-19 19:26:18 +0500

    starter project 01

---
+++ 01-starting-project/app.js
@@ -0,0 +1,17 @@
+Vue.createApp({
+  // old method
+  // data: function(){}
+  //modern method
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
+    },
+  },
+}).mount("#app");
